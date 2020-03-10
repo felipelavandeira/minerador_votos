ba685c1713168990345a16f07d6221e25a59882f
Felipe Lavandeira <felipe@localhost.localdomain>
2020-03-10 20:10:54 -0300

    Criando requisições para intervalos de anos ou mandatos

--- __main__.py
+++ __main__.py
@@ -2,13 +2,18 @@ from services.proposition_service import PropositionService
 
 
 def main():
-    service = PropositionService(2019)
-    proposition_list = service.get_from_ws()
 
-    for item in proposition_list:
-        print(type(item))
+    proposition_list = []
+    ano_inicial, ano_final = input('Selecione o ano inicial e o ano final para a seleção das requisições: ').split()
+    intervalo = range(int(ano_inicial), int(ano_final), 1)
 
-    print(len(proposition_list))
+    print('Buscando proposições...')
+
+    for ano in intervalo:
+        service = PropositionService(int(ano))
+        year_list = service.get_from_ws()
+        print('Encontradas {} proposições votadas no ano de {}'.format(len(year_list), ano))
+        proposition_list.append(year_list)
 
 
 if __name__ == "__main__":
